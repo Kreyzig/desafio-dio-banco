73b6ec67032387943f7e6260b738c710d2b2856a
Vanilson <vanilson@NOTEVK.progressul.local>
2022-08-15 14:48:39 -0300

    Java OO - Heranca
    Implementação de heranças, e interfaces

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,33 +1,4 @@
 
-public class ContaPoupanca {
-	private int agencia;
-	private int numero;
-	private double saldo;
-	
-	
-	public void sacar() {
-		
-	}
-	
-	public void depositar() {
-		
-	}
-	
-	public void transferir() {
-		
-	}
+public class ContaPoupanca extends Conta {
 
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-	
-	
 }
